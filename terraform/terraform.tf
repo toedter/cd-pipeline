e5638de5f45aaf8d91b86672fa37b95c54011f7e
Kai Toedter <kai@toedter.com>
2017-09-23 20:16:48 +0200

    better Jenkins config including security and admin user

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -68,7 +68,7 @@ resource "aws_instance" "web" {
   }
 
   provisioner "file" {
-    source      = "../docker/"
+    source      = "../docker"
     destination = "~"
   }
 
@@ -80,9 +80,10 @@ resource "aws_instance" "web" {
       "sudo apt-get update",
       "sudo apt-get -y install docker-ce",
       "sudo docker --version",
-      "curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > docker-compose",
+      "curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` > docker-compose",
       "sudo mv docker-compose /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
+      "cd docker",
       "sudo docker-compose build",
       "sudo docker-compose up -d",
     ]
